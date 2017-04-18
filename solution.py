89ee73b96e0cd96b5f6bee618bdfd7c66e34e3f3
minkvsky <chuanglongwei@163.com>
2017-04-19 01:44:36 +0900

    move global variable behind functions

--- solution.py
+++ solution.py
@@ -1,17 +1,3 @@
-assignments = []
-rows = 'ABCDEFGHI'
-cols = '123456789'
-boxes = cross(rows, cols)
-row_units = [cross(r, cols) for r in rows]
-column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI')
-                for cs in ('123', '456', '789')]
-diagonal_units = [[r + c for r, c in zip(rows,cols)],
-                  [r + c for r, c in zip(rows[::-1],cols)]]
-unitlist = row_units + column_units + square_units
-units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
-
 
 def assign_value(values, box, value):
     """
@@ -172,6 +158,20 @@ def solve(grid):
     peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
     values_solved = search(values)
     return values_solved
+
+assignments = []
+rows = 'ABCDEFGHI'
+cols = '123456789'
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI')
+                for cs in ('123', '456', '789')]
+diagonal_units = [[r + c for r, c in zip(rows,cols)],
+                  [r + c for r, c in zip(rows[::-1],cols)]]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
